31dd81e8adf3b52f83e3d3a743d85dcf22c69bca
Ambuj Kumar <ambujkumar22@gmail.com>
2023-09-26 16:02:05 +0530

    ch 03

---
+++ JS/01-basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = true
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abcd" => NaN
+// true/false => 1/0
+// undefined => NaN
+// null => 0
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false 
+// "abcd" => true
+// null => false
+// undefined => false
+
+let someNumber = 3
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
