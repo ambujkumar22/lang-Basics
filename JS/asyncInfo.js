0b093bf4b5f3c6d276a1c95f2dc48cf64b723421
Ambuj Kumar <ambujkumar22@gmail.com>
2023-10-06 01:23:16 +0530

    ch 35 to ch 38

---
+++ JS/asyncInfo.js
@@ -0,0 +1,19 @@
+/**
+// Javascript
+    1. Synchronus
+    2. Single Threaded
+
+// Execution Context
+    - execute one line of code at a time
+    - each operation waits for the last one to complete before executing
+
+// Blocking v/s Non-Blocking Code
+    Blocking
+        - block the flow of program
+        - Read File Sync
+
+    Non-Blocking
+        - does not block execution
+        - Read File Async
+
+*/
